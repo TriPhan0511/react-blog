ff22ccc782468ec8b09fc4f50a436420f125c516
Tri Phan <phanphutri@gmail.com>
2022-12-21 18:01:16 +0700

    Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Header from './Header'
+import Nav from './Nav'
+import Footer from './Footer'
+import Home from './Home'
+import NewPost from './NewPost'
+import PostPage from './PostPage'
+import About from './About'
+import Missing from './Missing'
+import { Route, Switch, useHistory } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
 function App() {
-  return <h1>Hello</h1>
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  )
 }
 
 export default App
